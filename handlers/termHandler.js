80ce23bb0c79aed900e13dac1c193e8892f8d650
Kiran Bhatt <kb_43@outlook.com>
2019-02-03 00:05:07 +0545

    Changed params to query for query string

--- handlers/termHandler.js
+++ handlers/termHandler.js
@@ -4,8 +4,8 @@ const termHandler = {};
 termHandler.getOutput = async (req, res) => {
 	console.log(req.url);
 	try {
-		const command = req.params.command || "echo";
-		const parameters = req.params.parameters || ["Hello World"];
+		const command = req.query.command || "echo";
+		const parameters = req.query.parameters || ["Hello World"];
 		let output = await terminal(command, parameters);
 		res.status(200).send(output);
 	} catch (err) {
