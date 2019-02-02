9ba52b7c3dfacc60a686144c1d9d322449aa6468
Roshan Bista <rrb_43@outlook.com>
2019-02-02 23:16:19 +0545

    Added params as, GET request, instead of body

--- handlers/termHandler.js
+++ handlers/termHandler.js
@@ -4,8 +4,8 @@ const termHandler = {};
 termHandler.getOutput = async (req, res) => {
 	console.log(req.url);
 	try {
-		const command = req.body.command || "echo Hello world";
-		const parameters = req.body.parameters || {};
+		const command = req.params.command || "echo";
+		const parameters = req.params.parameters || ["Hello World"];
 		let output = await terminal(command, parameters);
 		res.status(200).send(output);
 	} catch (err) {
